61492e2cf3856d2b5b4c6f1a0272bb3fd4337ebd
Md-Monirul-Islam <md.monirulmunna.cse@gmail.com>
2025-08-24 13:29:46 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,55 @@
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
+
+app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get('/posts')
+def get_posts():
+    return {"data": "This is your posts data"}
+
+
+@app.post('/create-post')
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    # return {"message": "Post created successfully", "data": payload}
+    return {'new post': f'title: {payload["title"]} content: {payload["content"]}'}
+
+
+@app.post('/create-new-post')
+def new_post(new_post: Post):
+    print(new_post.rating)
+    print(new_post.dict())
+    # return {'new post': f'title: {new_post.title} content: {new_post.content} published: {new_post.published} rating: {new_post.rating}'}
+    return {'new post': new_post}
+
+
+my_post = [{'title': 'title of post 1', 'content': 'content of post 1', 'id': 1},
+           {'title': 'title of post 2', 'content': 'content of post 2', 'id': 2}]
+
+@app.get('/my-post')
+def get_my_post():
+    return {'data': my_post}
+
+
+@app.post('/again-post')
+def again_post(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_post.append(post_dict)
+    return {'data': post_dict}
